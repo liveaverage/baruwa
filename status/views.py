44045515a9b1a9507b6e1a1a6be6b5b7d15e129d
Andrew Colin Kissa <andrew@topdog.za.net>
2010-03-31 15:05:52 +0000

    - up status funcs

--- status/views.py
+++ status/views.py
@@ -19,8 +19,10 @@
 from django.shortcuts import render_to_response
 from django.db import connection
 from baruwa.reports.views import r_query,raw_user_filter
+from baruwa.messages.process_mail import get_config_option
 from django.views.decorators.cache import never_cache
 from django.contrib.auth.decorators import login_required
+import os,subprocess
 
 @never_cache
 @login_required
@@ -49,5 +51,20 @@ def index(request):
         sql = raw_user_filter(request.user,user_type,addresses)
         c.execute(q+" AND "+sql)
     row = c.fetchone()
+    v1, v2, v3 = os.getloadavg()
+    load = "%.2f %.2f %.2f" % (v1,v2,v3)
+    p1 = subprocess.Popen('ps ax',shell=True,stdout=subprocess.PIPE)
+    p2 = subprocess.Popen('grep -i Mailscanner',shell=True,stdin=p1.stdout,stdout=subprocess.PIPE)
+    p3 = subprocess.Popen('grep -v grep',shell=True,stdin=p2.stdout,stdout=subprocess.PIPE)
+    p4 = subprocess.Popen('wc -l',shell=True,stdin=p2.stdout,stdout=subprocess.PIPE)
+    scanners = p4.stdout.read()
+    scanners = int(scanners.strip())
+    ms = get_config_option('MTA')
+    p1 = subprocess.Popen('ps ax',shell=True,stdout=subprocess.PIPE)
+    p2 = subprocess.Popen('grep -i '+ms,shell=True,stdin=p1.stdout,stdout=subprocess.PIPE)
+    p3 = subprocess.Popen('grep -v grep',shell=True,stdin=p2.stdout,stdout=subprocess.PIPE)
+    p4 = subprocess.Popen('wc -l',shell=True,stdin=p2.stdout,stdout=subprocess.PIPE)
+    mta = p4.stdout.read()
+    mta = int(mta.strip())
     data = {'total':row[0],'clean':row[1],'virii':row[2],'infected':row[3],'otherinfected':row[4],'spam':row[5],'highspam':row[6]}
-    return render_to_response('status/index.html',{'data':data,'user':request.user})
+    return render_to_response('status/index.html',{'data':data,'load':load,'scanners':scanners,'mta':mta,'user':request.user})
