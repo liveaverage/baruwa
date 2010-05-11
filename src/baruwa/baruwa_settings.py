400772ae924dae2d0195dc5b58cba9a95501f197
Andrew Colin Kissa <andrew@topdog.za.net>
2010-05-11 11:36:53 +0000

    - remove localized settings

--- src/baruwa/baruwa_settings.py
+++ src/baruwa/baruwa_settings.py
@@ -3,9 +3,8 @@
 
 MS_CONFIG = '/etc/MailScanner/MailScanner.conf'
 QUARANTINE_DAYS_TO_KEEP = 60
-QUARANTINE_REPORT_HOSTURL = 'http://baruwa-alpha.sentechsa.net'
+QUARANTINE_REPORT_HOSTURL = 'http://baruwa-alpha.local'
 MAIL_AUTH_HOSTS = (
-    ['sentechsa.net','cgp3.sentechsa.net','110','pop3',False],
-    ['topdog.za.net','tdss.co.za','25','smtp',True],
+    #['topdog.za.net','tdss.co.za','25','smtp',True],
 )
 SA_RULES_DIRS = ['/usr/share/spamassassin','/etc/mail/spamassassin']
