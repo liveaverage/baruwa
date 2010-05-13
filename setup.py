71f9fa31f16b0f291a69769861eb4941b36b8c16
Andrew Colin Kissa <andrew@topdog.za.net>
2010-05-13 07:16:26 +0000

    - Prepared update to rc1

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='baruwa',
-      version="0.0.1b",
+      version="0.0.1rc1",
       description="Ajax enabled MailScanner web frontend",
       long_description=read('README'),
       keywords='MailScanner Email Filters Quarantine Spam',
