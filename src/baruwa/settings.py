17f09d7828ad87779bee8a12c0dd7abf8a6f9ebd
Andrew Colin Kissa <andrew@topdog.za.net>
2010-07-02 11:19:25 +0000

    - remove csrf tags for django 1.1 compart

--- src/baruwa/settings.py
+++ src/baruwa/settings.py
@@ -81,7 +81,6 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',
     'baruwa.auth',
     'baruwa.accounts',
     'baruwa.messages',
