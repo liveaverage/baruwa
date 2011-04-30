85b4d69faa645b7d645fd11c93cea6ab682711db
Andrew Colin Kissa <andrew@topdog.za.net>
2011-04-30 11:26:57 +0200

    * added docstrings to base module

--- src/baruwa/__init__.py
+++ src/baruwa/__init__.py
@@ -18,3 +18,36 @@
 #
 # vim: ai ts=4 sts=4 et sw=4
 #
+
+"""
+======
+Baruwa
+======
+Baruwa (swahili for letter or mail) is a web 2.0 `MailScanner`_ front-end. 
+
+It provides an easy to use interface for managing a MailScanner installation. It is used to
+perform operations such as releasing quarantined messages, spam learning, whitelisting and 
+blacklisting addresses, monitoring the health of the services etc. Baruwa is implemented 
+using web 2.0 features (AJAX) where deemed fit, graphing is also implemented on the client
+side using SVG, Silverlight or VML. Baruwa has full support for i18n, letting you support 
+any language of your choosing.
+
+It includes reporting functionality with an easy to use query builder, results can be 
+displayed as message lists or graphed as colorful and pretty interactive graphs.
+
+Custom MailScanner modules are provided to allow for logging of messages to the mysql
+database with SQLite as backup, managing whitelists and blacklists and managing per
+user spam check settings.
+
+Baruwa is open source software, written in Python/Perl using the Django Framework and 
+MySQL for storage, it is released under the GPLv2 and is available for free download.
+
+============
+Installation
+============
+Baruwa is installed in the usual way::
+
+    python setup.py install
+
+.. _`MailScanner`: http://www.mailscanner.info/
+"""
