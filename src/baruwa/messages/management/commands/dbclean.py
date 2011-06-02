52476c9b9ab2b72c54cb2e9dc929cb391466ad0f
Andrew Colin Kissa <andrew@topdog.za.net>
2011-06-02 13:21:31 +0200

    * DBclean up based on QUARANTINE_DAYS_TO_KEEP  * FIX: Errors caused by interupted cleanu

--- src/baruwa/messages/management/commands/dbclean.py
+++ src/baruwa/messages/management/commands/dbclean.py
@@ -1,17 +1,17 @@
-# 
+#
 # Baruwa - Web 2.0 MailScanner front-end.
 # Copyright (C) 2010  Andrew Colin Kissa <andrew@topdog.za.net>
-# 
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
@@ -19,11 +19,15 @@
 # vim: ai ts=4 sts=4 et sw=4
 #
 
+from django.conf import settings
 from django.core.management.base import NoArgsCommand
+from django.utils.translation import ugettext as _
+
 
 class Command(NoArgsCommand):
     "Archive messages and delete from messages table"
-    help = "Deletes records older than 60 days from the messages table"
+    help = _("Deletes records older than QUARANTINE_DAYS_TO_KEEP"
+    " days from the messages table")
 
     def handle_noargs(self, **options):
         from django.db import connection
@@ -32,17 +36,27 @@ class Command(NoArgsCommand):
         #interval = datetime.timedelta(days=60)
         #last_date = datetime.datetime.now() - interval
         #Message.objects.filter(timestamp__lt=last_date).delete()
+        days = getattr(settings, 'QUARANTINE_DAYS_TO_KEEP', 60)
 
         conn = connection.cursor()
         conn.execute(
-            """INSERT LOW_PRIORITY INTO archive 
-            SELECT * FROM messages WHERE timestamp < 
-            DATE_SUB(CURDATE(), INTERVAL 60 DAY)"""
+            """DELETE FROM messages WHERE id in 
+            (SELECT id FROM archive WHERE timestamp <
+            DATE_SUB(CURDATE(), INTERVAL %s DAY))
+            """ % str(days)
         )
         conn.execute(
-            """DELETE LOW_PRIORITY FROM messages 
-            WHERE timestamp < DATE_SUB(CURDATE(), 
-            INTERVAL 60 DAY)"""
+            """INSERT LOW_PRIORITY INTO archive
+            SELECT * FROM messages WHERE timestamp <
+            DATE_SUB(CURDATE(), INTERVAL %s DAY)
+            """ % str(days)
+            
+        )
+        conn.execute(
+            """DELETE LOW_PRIORITY FROM messages
+            WHERE timestamp < DATE_SUB(CURDATE(),
+            INTERVAL %s DAY)
+            """ % str(days)
         )
         conn.execute('OPTIMIZE TABLE messages')
         conn.execute('OPTIMIZE TABLE archive')
