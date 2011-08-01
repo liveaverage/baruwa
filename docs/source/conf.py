0598eec4dc9319ee77a7d67191c1dbc01c6b3007
Andrew Colin Kissa <andrew@topdog.za.net>
2011-08-01 23:34:23 +0200

    * documentation updates

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -162,6 +162,7 @@ html_static_path = ['_static']
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'Baruwadoc'
+html_add_permalinks = False
 
 
 # -- Options for LaTeX output -------------------------------------------------
